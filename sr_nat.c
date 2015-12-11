65d67d7f9970f43c6bf1a0bb7561e7b0231c5591
Daniel Han <zhixu.han@mail.utoronto.ca>
2015-12-11 00:19:53 -0800

    modify unsolicited and etc.

--- sr_nat.c
+++ sr_nat.c
@@ -120,6 +120,9 @@ void *sr_nat_timeout(void *sr_ptr) {  /* Periodic Timout handling */
         }
         /* otherwise we need to loop through the packets */
         else {
+			
+			struct sr_tcp_unsolicited_packet *prev_pkt = my_pkt;
+
             while (next_pkt != NULL) {
 
                 time_t pkt_time = my_pkt->time_updated;
@@ -169,16 +172,69 @@ void *sr_nat_timeout(void *sr_ptr) {  /* Periodic Timout handling */
                     /* unlist the my_pkt */
                     if (my_pkt == nat->unsolicited_packet) {
                         nat->unsolicited_packet = next_pkt;
-                        my_pkt = next_pkt;
+                        prev_pkt = next_pkt;
+						my_pkt = next_pkt;
                         next_pkt = next_pkt->next;
                     } else {
+                        prev_pkt->next = next_pkt;
                         my_pkt = next_pkt;
-                        next_pkt = next_pkt->next;
+						next_pkt = next_pkt->next;
                     }
                 }
+				prev_pkt = my_pkt;
+				my_pkt = next_pkt;
+				next_pkt = next_pkt->next;
             }
-            pthread_mutex_unlock(&(nat->lock));
-        }
+			time_t pkt_time = my_pkt->time_updated;
+
+			/* if the time difference is bigger than 6 seconds */
+            if (difftime(curtime, pkt_time) >= 6) {
+                /* get all the headers */
+                uint8_t *packet = my_pkt->buf;
+                sr_ethernet_hdr_t *eth_hdr = get_eth_hdr(packet);
+                sr_ip_hdr_t *ip_hdr = get_ip_hdr(packet);
+
+                /* create a new icmp t3 port unreachable */
+                int packet_len = ICMP_T3_PACKET_LEN;
+                uint8_t *icmp_t3_hdr = (uint8_t *)malloc(packet_len);
+                /* create ethernet header */
+                sr_ethernet_hdr_t *new_eth_hdr = (sr_ethernet_hdr_t *)icmp_t3_hdr;
+                memcpy(new_eth_hdr->ether_dhost, eth_hdr->ether_shost, ETHER_ADDR_LEN);
+                memcpy(new_eth_hdr->ether_shost, eth_hdr->ether_dhost, ETHER_ADDR_LEN);
+                /* create ip header */
+                sr_ip_hdr_t *new_ip_hdr = (sr_ip_hdr_t *)((char *)icmp_t3_hdr + ETHER_PACKET_LEN);
+                new_ip_hdr->ip_hl = ip_hdr->ip_hl;          /* header length */
+                new_ip_hdr->ip_v = ip_hdr->ip_v;            /* header version */
+                new_ip_hdr->ip_tos = ip_hdr->ip_tos;        /* type of service */
+                new_ip_hdr->ip_len = htons(56);             /* total length */
+                new_ip_hdr->ip_id = 0;              /* identification */
+                new_ip_hdr->ip_off = htons(0b0100000000000000);        /* fragment offset field */
+                new_ip_hdr->ip_ttl = 64;                    /* time to live */
+                new_ip_hdr->ip_p = ip_protocol_icmp;            /* protocol */
+                new_ip_hdr->ip_src =  ip_hdr->ip_dst;       /* source address */
+                new_ip_hdr->ip_dst = ip_hdr->ip_src;        /* dest address */
+                new_ip_hdr->ip_sum = 0;
+                new_ip_hdr->ip_sum = cksum(new_ip_hdr, sizeof(sr_ip_hdr_t));;   /* checksum */
+                /* create icmp t3 header */
+                sr_icmp_t3_hdr_t *new_icmp_t3_hdr = (sr_icmp_t3_hdr_t *)((char *)icmp_t3_hdr + IP_PACKET_LEN);
+                new_icmp_t3_hdr->icmp_type = htons(3);
+                new_icmp_t3_hdr->icmp_code = htons(3);
+                new_icmp_t3_hdr->unused = 0;
+                new_icmp_t3_hdr->next_mtu = 0;
+                memcpy(new_icmp_t3_hdr->data, new_ip_hdr, ICMP_DATA_SIZE); 
+                new_icmp_t3_hdr->icmp_sum = 0;
+                new_icmp_t3_hdr->icmp_sum = cksum(new_icmp_t3_hdr, sizeof(sr_icmp_t3_hdr_t));
+
+                struct sr_if *out_iface = sr_get_router_if(sr, ip_hdr->ip_dst);
+
+				sr_send_packet(sr, icmp_t3_hdr, packet_len, out_iface->name);
+
+				/* set the last packet to be NULL */
+                prev_pkt->next = NULL;
+
+			}
+		pthread_mutex_unlock(&(nat->lock));
+        }		
 	}
   }
   return NULL;
@@ -467,5 +523,71 @@ struct sr_tcp_unsolicited_packet *sr_nat_unsolicited_queue(struct sr_nat *nat, u
 	return my_pkt;
 
 }
+
+
+/* when an outbound packet comes, check the cached packets, 
+	if ip_dst and port_server matches, drop the cached unsolicited packets
+ */
+void drop_unsolicited_packet(struct sr_nat *nat, uint8_t *packet) {
+	pthread_mutex_lock(&(nat->lock));
+	
+	/* get ip hdr and tcp hdr */
+	sr_ip_hdr_t *ip_hdr = get_ip_hdr(packet);
+	sr_tcp_hdr_t *tcp_hdr = get_tcp_hdr(packet);
+
+	struct sr_tcp_unsolicited_packet *my_pkt = nat->unsolicited_packet;
+
+	if (my_pkt != NULL) {
+		struct sr_tcp_unsolicited_packet *next_pkt = my_pkt->next;
+		
+		if (next_pkt == NULL) {
+			sr_ip_hdr_t *my_pkt_ip_hdr = get_ip_hdr(my_pkt->buf);
+			sr_tcp_hdr_t *my_pkt_tcp_hdr = get_tcp_hdr(my_pkt->buf);
+
+			if (ip_hdr->ip_dst == my_pkt_ip_hdr->ip_src && tcp_hdr->dst_port == my_pkt_tcp_hdr->src_port) {
+				nat->unsolicited_packet = NULL;
+			}
+		}
+		else {
+			struct sr_tcp_unsolicited_packet *prev_pkt = my_pkt;
+	
+			while (next_pkt != NULL) {
+				sr_ip_hdr_t *my_pkt_ip_hdr = get_ip_hdr(my_pkt->buf);
+				sr_tcp_hdr_t *my_pkt_tcp_hdr = get_tcp_hdr(my_pkt->buf);
+
+				if (ip_hdr->ip_dst == my_pkt_ip_hdr->ip_src && tcp_hdr->dst_port == my_pkt_tcp_hdr->src_port) {
+					if (my_pkt == nat->unsolicited_packet) {
+						nat->unsolicited_packet = next_pkt;
+						prev_pkt = next_pkt;
+						my_pkt = next_pkt;
+						next_pkt = next_pkt->next;
+					} else {
+						prev_pkt->next = next_pkt;
+						my_pkt = next_pkt;
+						next_pkt = next_pkt->next;
+					}
+				}
+				prev_pkt = my_pkt;
+				my_pkt = next_pkt;
+				next_pkt = next_pkt->next;
+			}
+			/* check the last packet */
+			sr_ip_hdr_t *my_pkt_ip_hdr = get_ip_hdr(my_pkt->buf);
+			sr_tcp_hdr_t *my_pkt_tcp_hdr = get_tcp_hdr(my_pkt->buf);
+			if (ip_hdr->ip_dst == my_pkt_ip_hdr->ip_src && tcp_hdr->dst_port == my_pkt_tcp_hdr->src_port) {
+				prev_pkt->next = NULL;
+			}
+		}
+	}
+	
+	pthread_mutex_unlock(&(nat->lock));
+
+	return;
+}
+
+
+
+
+
 
 
